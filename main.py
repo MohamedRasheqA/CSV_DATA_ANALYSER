48e07adc03ce54ab19cee054a7db6cbb31cd726a
MohamedRasheqA <rasheq@klmsolutions.in>
2024-11-24 12:06:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ async def main():
                 anthropic = Anthropic()
                 st.write("Generating analysis...")
                 msg = anthropic.messages.create(
-                    model='claude-3-5-sonnet-20240620',
+                    model='claude-3-haiku-20240307',
                     max_tokens=1024,
                     messages=[
                         {"role": "user", "content": prompt}
